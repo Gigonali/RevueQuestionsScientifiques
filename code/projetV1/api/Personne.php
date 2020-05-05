e5368df6a8e2f56365506badb82e37dd047e81b1
Lepoivre <la188101@student.helha.be>
2020-05-05 19:39:31 +0200

    a

--- code/projetV1/api/Personne.php
+++ code/projetV1/api/Personne.php
@@ -1,140 +1,41 @@
 <?php
+  require_once('./manager/DBManager.php');
+  require_once('./model/Personne.Class.php');
+  require_once('./manager/PersonneManager.php');
 
-  class RevueManager {
-    private $connexion;
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json;');
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+  header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization');
 
-    function __construct($db) {
-      $this->connexion = $db;
+  $dbManager = new DBManager();
+  $connexion = $dbManager->connect();
+  $personneManager = new PersonneManager($connexion);
+
+  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    
+    if (isset($_GET['total'])) {
+      echo json_encode($personneManager->total());
+    } else {
+      if (isset($_GET['id'])) {
+        $idToGet = $_GET['id'];
+        echo json_encode($personneManager->get($idToGet));
+      } else echo json_encode($personneManager->getAll());
     }
 
-    function get($idRevue) {
-      $prep = null;
-      $revue = null;
+  } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    echo 'not implemented yet';
 
-      try {
-        $sql = "SELECT * FROM revue WHERE `id_rev`=:idRevue";
-        $prep = $this->connexion->prepare($sql);
-        $prep->bindValue(':idRevue', $idRevue, PDO::PARAM_INT);
-        $prep->execute();
-        $result = $prep->fetch(PDO::FETCH_ASSOC);
+  } else if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    $personneJSON = json_decode(file_get_contents('php://input'), true);
+    $personne = new Personne($personneJSON);
+    echo json_encode($personneManager->ajouter($personne));
 
-        $revue = new Revue($result);
-
-      } catch (PDOException $e) {
-        die($e);
-      } finally {
-        $prep->closeCursor();
-        $prep = null;
-      }
-
-      return $revue;
-
-    }
-
-    function getAll() {
-      $prep = null;
-      $revue = null;
-      $revues = array();
-
-      try {
-        $sql = "SELECT * FROM revue ORDER BY numero_rev";
-        $prep = $this->connexion->prepare($sql);
-        $prep->execute();
-        $result = $prep->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($result as $revueDB) {
-          $revue = new Revue($revueDB);
-          array_push($revues, $revue);
-        }
-
-      } catch (PDOException $e) {
-        die($e);
-      } finally {
-        $prep->closeCursor();
-        $prep = null;
-      }
-
-      return $revues;
-
-    }
-
-    function total() {
-      return $this->connexion->query('SELECT COUNT(*) FROM revue')->fetchColumn();
-    }
-
-    function ajouter($revue) {
-      $prep = null;
-
-      // vérifie si elle n'existe pas déjà
-      $sql = "SELECT * FROM revue WHERE `numero_rev`=:numeroRevue";
-      $prep = $this->connexion->prepare($sql);
-      $prep->bindValue(':numeroRevue', $revue->__get('numero_rev'), PDO::PARAM_INT);
-      $prep->execute();
-      $result = $prep->fetchAll(PDO::FETCH_ASSOC);
-
-      if (!$result) {
-        try {
-          $sql = "INSERT INTO `revue`(`id_rev`, `numero_rev`, `special_helha_rev`) VALUES (NULL, :numero, :specialHELHa)";
-          $prep = $this->connexion->prepare($sql);
-          $prep->bindValue(':numero', $revue->__get('numero_rev'), PDO::PARAM_INT);
-          $prep->bindValue(':specialHELHa', $revue->__get('special_helha_rev'), PDO::PARAM_BOOL);
-          $result = $prep->execute();
-
-        } catch (PDOException $e) {
-          die($e);
-        } finally {
-          $prep->closeCursor();
-          $prep = null;
-        }
-      } else $result = false;
-
-      return $result;
-
-    }
-
-    function modifier($revue) {
-      $prep = null;
-      $result = null;
-
-      try {
-        $sql = "UPDATE revue SET `numero_rev`=:numero, `special_helha_rev`=:specialHELHa WHERE `id_rev`=:idRevue";
-        $prep = $this->connexion->prepare($sql);
-        $prep->bindValue(':idRevue', $revue->__get('id_rev'), PDO::PARAM_INT);
-        $prep->bindValue(':numero', $revue->__get('numero_rev'), PDO::PARAM_INT);
-        $prep->bindValue(':specialHELHa', $revue->__get('special_helha_rev'), PDO::PARAM_BOOL);
-        $result = $prep->execute();
-
-      } catch (PDOException $e) {
-        die($e);
-      } finally {
-        $prep->closeCursor();
-        $prep = null;
-      }
-
-      return $result;
-
-    }
-
-    function supprimer($id_revRevue) {
-      $prep = null;
-      $result = null;
-
-      try {
-        $sql = "DELETE FROM revue WHERE `id_rev`=:idRevue";
-        $prep = $this->connexion->prepare($sql);
-        $prep->bindValue(':idRevue', $id_revRevue, PDO::PARAM_INT);
-        $result = $prep->execute();
-
-      } catch (PDOException $e) {
-        die($e);
-      } finally {
-        $prep->closeCursor();
-        $prep = null;
-      }
-
-      return $result;
-    }
+  } else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    $idToDelete = $_GET['id'];
+    echo $personneManager->supprimer($idToDelete);
 
   }
+
 
 ?>
