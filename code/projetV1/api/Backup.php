b56bea89f577a0ba7aebd6ff895db86f3d801c87
MikeRenauld <MikeRenauld@users.noreply.github.com>
2020-05-08 22:16:45 +0200

    Modifications dans le tab-maintenance.component et le backend
    - déplacement des fichiers maintenance.service et date-custom.pipe dans les dossiers services et pipes + adaptation dans l'app.module
    
    tab-maintenance.component :
    - ajout d'une div de résultat par rapport à l'export
    
    date-custom.pipe :
    - Modification du fonctionnement pour retourner la date
    
    API Backup :
    - Modification du fonctionnement de la fonction createBackup
    - création d'un dossier Backup et déplacement du fichier log.txt dedans
    
    Ajout d'un script php externe Dumper

--- code/projetV1/api/Backup.php
+++ code/projetV1/api/Backup.php
@@ -7,28 +7,19 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization');
 
-
 $dbManager = new DBManager();
 $connexion = $dbManager->connect();
 $backupManager = new BackupManager($connexion);
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
 
-    $file = $backupManager->createBackup();
-    if(file_exists($file)){
-      header('Content-Description: File Transfer');
-      header('Content-Type: application/octet-stream');
-      header('Content-Disposition: attachment; filename="'.basename($file).'"');
-      header('Expires: 0');
-      header('Cache-Control: must-revalidate');
-      header('Pragma: public');
-      header('Content-Length: ' . filesize($file));
-      readfile($file);
-      exit;
+    if (isset($_GET['date'])) {
+      echo json_encode($backupManager->getDateLastBackup());
+    } else {
+      echo json_encode($backupManager->createBackup());
+
     }
 
-}
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-  echo json_encode($backupManager->getDateLastBackup());
+} else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  echo 'not implemented';
 }
